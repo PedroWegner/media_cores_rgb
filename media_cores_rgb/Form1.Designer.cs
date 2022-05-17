3651221d5216f7b227b3c71885d8f8b7f170428f
Pedro Henrique Wegner <pedrowegner@gmail.com>
2022-05-17 15:05:29 -0300

    Initial commit

---
+++ media_cores_rgb/Form1.Designer.cs
@@ -0,0 +1,115 @@
+﻿namespace media_cores_rgb
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.pictureWeb = new System.Windows.Forms.PictureBox();
+            this.comboWeb = new System.Windows.Forms.ComboBox();
+            this.startButton = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureWeb)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer1.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer1.Name = "splitContainer1";
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.pictureWeb);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.startButton);
+            this.splitContainer1.Panel2.Controls.Add(this.comboWeb);
+            this.splitContainer1.Size = new System.Drawing.Size(1179, 559);
+            this.splitContainer1.SplitterDistance = 947;
+            this.splitContainer1.TabIndex = 0;
+            // 
+            // pictureWeb
+            // 
+            this.pictureWeb.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pictureWeb.Location = new System.Drawing.Point(0, 0);
+            this.pictureWeb.Name = "pictureWeb";
+            this.pictureWeb.Size = new System.Drawing.Size(947, 559);
+            this.pictureWeb.TabIndex = 0;
+            this.pictureWeb.TabStop = false;
+            // 
+            // comboWeb
+            // 
+            this.comboWeb.FormattingEnabled = true;
+            this.comboWeb.Location = new System.Drawing.Point(3, 12);
+            this.comboWeb.Name = "comboWeb";
+            this.comboWeb.Size = new System.Drawing.Size(213, 24);
+            this.comboWeb.TabIndex = 0;
+            // 
+            // startButton
+            // 
+            this.startButton.Location = new System.Drawing.Point(2, 42);
+            this.startButton.Name = "startButton";
+            this.startButton.Size = new System.Drawing.Size(214, 23);
+            this.startButton.TabIndex = 1;
+            this.startButton.Text = "Start";
+            this.startButton.UseVisualStyleBackColor = true;
+            this.startButton.Click += new System.EventHandler(this.startButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1179, 559);
+            this.Controls.Add(this.splitContainer1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureWeb)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.PictureBox pictureWeb;
+        private System.Windows.Forms.Button startButton;
+        private System.Windows.Forms.ComboBox comboWeb;
+    }
+}
+
---
+++ media_cores_rgb/Form1.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using AForge.Video;
+using AForge.Video.DirectShow;
+
+namespace media_cores_rgb
+{
+    public partial class Form1 : Form
+    {
+        FilterInfoCollection fic;
+        VideoCaptureDevice vdc;
+        Bitmap bmp = null;
+        Graphics g = null;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+       
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // procura os perifericos capazes de capturar video
+            fic = new FilterInfoCollection(FilterCategory.VideoInputDevice);
+            foreach (FilterInfo filterInfo in fic)
+            {
+                comboWeb.Items.Add(filterInfo.Name);
+
+            }
+            comboWeb.SelectedIndex = 0;
+            vdc = new VideoCaptureDevice();
+        }
+
+        private void startButton_Click(object sender, EventArgs e)
+        {
+            vdc = new VideoCaptureDevice(fic[comboWeb.SelectedIndex].MonikerString);
+            vdc.NewFrame += VideoCaptureFrames;
+            vdc.Start();
+        }
+
+        private void VideoCaptureFrames(object sender, NewFrameEventArgs eventArgs)
+        {
+            pictureWeb.Image?.Dispose();
+            pictureWeb.Image = (Bitmap)eventArgs.Frame.Clone();
+
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (vdc.IsRunning == true)
+            {
+                vdc.Stop();
+            }
+        }
+    }
+}
