e49efa17dfd763a5c02c4c9f788935025acf80a1
Pedro Henrique Wegner <pedrowegner@gmail.com>
2022-05-17 16:17:20 -0300

    adicionado funcao para media de rgb

--- media_cores_rgb/Form1.Designer.cs
+++ media_cores_rgb/Form1.Designer.cs
@@ -30,8 +30,12 @@
         {
             this.splitContainer1 = new System.Windows.Forms.SplitContainer();
             this.pictureWeb = new System.Windows.Forms.PictureBox();
-            this.comboWeb = new System.Windows.Forms.ComboBox();
+            this.labelGreen = new System.Windows.Forms.Label();
+            this.labelBlue = new System.Windows.Forms.Label();
+            this.labelRed = new System.Windows.Forms.Label();
             this.startButton = new System.Windows.Forms.Button();
+            this.comboWeb = new System.Windows.Forms.ComboBox();
+            this.labelByte = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
             this.splitContainer1.Panel1.SuspendLayout();
             this.splitContainer1.Panel2.SuspendLayout();
@@ -51,10 +55,14 @@
             // 
             // splitContainer1.Panel2
             // 
+            this.splitContainer1.Panel2.Controls.Add(this.labelByte);
+            this.splitContainer1.Panel2.Controls.Add(this.labelGreen);
+            this.splitContainer1.Panel2.Controls.Add(this.labelBlue);
+            this.splitContainer1.Panel2.Controls.Add(this.labelRed);
             this.splitContainer1.Panel2.Controls.Add(this.startButton);
             this.splitContainer1.Panel2.Controls.Add(this.comboWeb);
             this.splitContainer1.Size = new System.Drawing.Size(1179, 559);
-            this.splitContainer1.SplitterDistance = 947;
+            this.splitContainer1.SplitterDistance = 946;
             this.splitContainer1.TabIndex = 0;
             // 
             // pictureWeb
@@ -62,17 +70,39 @@
             this.pictureWeb.Dock = System.Windows.Forms.DockStyle.Fill;
             this.pictureWeb.Location = new System.Drawing.Point(0, 0);
             this.pictureWeb.Name = "pictureWeb";
-            this.pictureWeb.Size = new System.Drawing.Size(947, 559);
+            this.pictureWeb.Size = new System.Drawing.Size(946, 559);
             this.pictureWeb.TabIndex = 0;
             this.pictureWeb.TabStop = false;
             // 
-            // comboWeb
+            // labelGreen
             // 
-            this.comboWeb.FormattingEnabled = true;
-            this.comboWeb.Location = new System.Drawing.Point(3, 12);
-            this.comboWeb.Name = "comboWeb";
-            this.comboWeb.Size = new System.Drawing.Size(213, 24);
-            this.comboWeb.TabIndex = 0;
+            this.labelGreen.AutoSize = true;
+            this.labelGreen.Font = new System.Drawing.Font("Arial Narrow", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelGreen.Location = new System.Drawing.Point(3, 101);
+            this.labelGreen.Name = "labelGreen";
+            this.labelGreen.Size = new System.Drawing.Size(52, 24);
+            this.labelGreen.TabIndex = 3;
+            this.labelGreen.Text = "label1";
+            // 
+            // labelBlue
+            // 
+            this.labelBlue.AutoSize = true;
+            this.labelBlue.Font = new System.Drawing.Font("Arial Narrow", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelBlue.Location = new System.Drawing.Point(3, 125);
+            this.labelBlue.Name = "labelBlue";
+            this.labelBlue.Size = new System.Drawing.Size(52, 24);
+            this.labelBlue.TabIndex = 2;
+            this.labelBlue.Text = "label1";
+            // 
+            // labelRed
+            // 
+            this.labelRed.AutoSize = true;
+            this.labelRed.Font = new System.Drawing.Font("Arial Narrow", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelRed.Location = new System.Drawing.Point(3, 68);
+            this.labelRed.Name = "labelRed";
+            this.labelRed.Size = new System.Drawing.Size(52, 24);
+            this.labelRed.TabIndex = 1;
+            this.labelRed.Text = "label1";
             // 
             // startButton
             // 
@@ -83,6 +113,24 @@
             this.startButton.Text = "Start";
             this.startButton.UseVisualStyleBackColor = true;
             this.startButton.Click += new System.EventHandler(this.startButton_Click);
+            // 
+            // comboWeb
+            // 
+            this.comboWeb.FormattingEnabled = true;
+            this.comboWeb.Location = new System.Drawing.Point(3, 12);
+            this.comboWeb.Name = "comboWeb";
+            this.comboWeb.Size = new System.Drawing.Size(213, 24);
+            this.comboWeb.TabIndex = 0;
+            // 
+            // labelByte
+            // 
+            this.labelByte.AutoSize = true;
+            this.labelByte.Font = new System.Drawing.Font("Arial Narrow", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelByte.Location = new System.Drawing.Point(2, 179);
+            this.labelByte.Name = "labelByte";
+            this.labelByte.Size = new System.Drawing.Size(52, 24);
+            this.labelByte.TabIndex = 4;
+            this.labelByte.Text = "label1";
             // 
             // Form1
             // 
@@ -97,6 +145,7 @@
             this.Load += new System.EventHandler(this.Form1_Load);
             this.splitContainer1.Panel1.ResumeLayout(false);
             this.splitContainer1.Panel2.ResumeLayout(false);
+            this.splitContainer1.Panel2.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
             this.splitContainer1.ResumeLayout(false);
             ((System.ComponentModel.ISupportInitialize)(this.pictureWeb)).EndInit();
@@ -110,6 +159,10 @@
         private System.Windows.Forms.PictureBox pictureWeb;
         private System.Windows.Forms.Button startButton;
         private System.Windows.Forms.ComboBox comboWeb;
+        private System.Windows.Forms.Label labelGreen;
+        private System.Windows.Forms.Label labelBlue;
+        private System.Windows.Forms.Label labelRed;
+        private System.Windows.Forms.Label labelByte;
     }
 }
 
--- media_cores_rgb/Form1.cs
+++ media_cores_rgb/Form1.cs
@@ -3,9 +3,11 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.Drawing.Imaging;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Runtime.InteropServices;
 using System.Windows.Forms;
 using AForge.Video;
 using AForge.Video.DirectShow;
@@ -14,17 +16,26 @@ namespace media_cores_rgb
 {
     public partial class Form1 : Form
     {
+        #region Variables
         FilterInfoCollection fic;
         VideoCaptureDevice vdc;
         Bitmap bmp = null;
-        Graphics g = null;
+        double somaR = 0;
+        double somaG = 0;
+        double somaB = 0;
+        double count = 0;
+
+
+
+
+        #endregion
 
         public Form1()
         {
             InitializeComponent();
         }
 
-       
+
 
         private void Form1_Load(object sender, EventArgs e)
         {
@@ -48,9 +59,41 @@ namespace media_cores_rgb
 
         private void VideoCaptureFrames(object sender, NewFrameEventArgs eventArgs)
         {
-            pictureWeb.Image?.Dispose();
-            pictureWeb.Image = (Bitmap)eventArgs.Frame.Clone();
 
+
+            bmp = (Bitmap)eventArgs.Frame.Clone();
+            var rect = new Rectangle(0, 0, bmp.Width, bmp.Height);
+            var data = bmp.LockBits(rect, ImageLockMode.ReadWrite, PixelFormat.Format24bppRgb);
+            var array = new byte[data.Height * data.Stride];
+            Marshal.Copy(data.Scan0, array, 0, array.Length);
+
+            somaB = 0;
+            somaG = 0;
+            somaR = 0;
+            for (int i = 0; i < array.Length; i += 3)
+            {
+                if (!(array[i + 0] < 190 && array[i + 1] < 190 && array[i + 2] < 190))
+                {
+                    array[i + 0] = 255;
+                    array[i + 1] = 0;
+                    array[i + 2] = 0;
+                }
+                somaB += array[i + 0];
+                somaG += array[i + 1];
+                somaR += array[i + 2];
+
+            }
+
+            labelBlue.Text = "Blue: " + (somaB / (array.Length / 3)).ToString();
+            labelGreen.Text = "Green: " + (somaG / (array.Length / 3)).ToString();
+            labelRed.Text = "Red: " + (somaR / (array.Length / 3)).ToString();
+            labelByte.Text = "Qtd bytes: " + (array.Length / 3).ToString();
+
+            Marshal.Copy(array, 0, data.Scan0, array.Length);
+            bmp.UnlockBits(data);
+
+            pictureWeb.Image?.Dispose(); // para liberar consumo de memoria
+            pictureWeb.Image = bmp;
         }
 
         private void Form1_FormClosing(object sender, FormClosingEventArgs e)
